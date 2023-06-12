85d9a035ca08dba4ffd2f4025a6be7b885c41512
Jaysmito101 <73700725+Jaysmito101@users.noreply.github.com>
2023-06-12 09:23:30 +0000

    cleaned up

---
+++ scripts/chatboxManager.js
@@ -0,0 +1,58 @@
+function addChatMessage(message, from) {
+	document.getElementById("message-container").innerHTML += `
+					<div id= "messageOutput" style="background-color: ${utilsGenerateColorFromString(from)};">
+						<div id= "messageOCH">
+							<header>
+								<h4>${from}
+									<!--- <time>${new Date()}</time> --->
+								</h4>
+							</header>
+							<p>${message}</p>
+						</div>
+					</div>
+                `;
+	let objDiv = document.getElementById("message-container");
+	objDiv.scrollTop = objDiv.scrollHeight;
+}
+
+function dragElement(elmnt) {
+	var pos1 = 0,
+		pos2 = 0,
+		pos3 = 0,
+		pos4 = 0;
+	if (document.getElementById(elmnt.id + "header")) {
+		/* if present, the header is where you move the DIV from:*/
+		document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+	} else {
+		/* otherwise, move the DIV from anywhere inside the DIV:*/
+		elmnt.onmousedown = dragMouseDown;
+	}
+
+	function dragMouseDown(e) {
+		e = e || window.event;
+		// get the mouse cursor position at startup:
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		document.onmouseup = closeDragElement;
+		// call a function whenever the cursor moves:
+		document.onmousemove = elementDrag;
+	}
+
+	function elementDrag(e) {
+		e = e || window.event;
+		// calculate the new cursor position:
+		pos1 = pos3 - e.clientX;
+		pos2 = pos4 - e.clientY;
+		pos3 = e.clientX;
+		pos4 = e.clientY;
+		// set the element's new position:
+		elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+		elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+	}
+
+	function closeDragElement() {
+		/* stop moving when mouse button is released:*/
+		document.onmouseup = null;
+		document.onmousemove = null;
+	}
+}
